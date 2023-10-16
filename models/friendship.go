20d4bdd60c2d6f68b12dd2e79769e5cda990e508
Grandbusta <bustajay30@gmail.com>
2023-10-17 00:21:57 +0100

    relationships init

---
+++ models/friendship.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// Status type= pending, accepted, declined
+
+type Friendship struct {
+	ID        primitive.ObjectID `bson:"_id, omitempty" json:"id"`
+	Sender    primitive.ObjectID `bson:"sender" json:"sender"`
+	Receiver  primitive.ObjectID `bson:"receiver" json:"receiver"`
+	Status    string             `bson:"status" json:"status"`
+	CreatedAt time.Time          `bson:"createdAt" json:"createdAt"`
+	UpdatedAt time.Time          `bson:"updatedAt" json:"updatedAt"`
+}
--- models/user.go
+++ models/user.go
@@ -1,12 +1,18 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type User struct {
-	ID       primitive.ObjectID `bson:"_id, omitempty" json:"id"`
-	Email    string             `json:"email"`
-	Password string             `json:"password"`
-	Username string             `json:"username"`
+	ID        primitive.ObjectID `bson:"_id, omitempty" json:"id"`
+	Email     string             `json:"email"`
+	Password  string             `json:"password"`
+	Username  string             `json:"username"`
+	CreatedAt time.Time          `bson:"createdAt" json:"createdAt"`
+	UpdatedAt time.Time          `bson:"updatedAt" json:"updatedAt"`
 }
 
 type UserInput struct {
